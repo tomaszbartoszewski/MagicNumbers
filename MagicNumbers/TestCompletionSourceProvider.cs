17293ea76c0d25a2b8ccce4de4c500e9574fbcc5
tomaszbartoszewski <tomasz.bartoszewski@gmail.com>
2016-12-02 21:25:54 +0000

    Add intellisense

---
+++ MagicNumbers/TestCompletionSourceProvider.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.Language.Intellisense;
+using Microsoft.VisualStudio.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Operations;
+
+namespace MagicNumbers
+{
+    [Export(typeof(ICompletionSourceProvider))]
+    [ContentType("plaintext")]
+    [Name("token completion")]
+    internal class TestCompletionSourceProvider : ICompletionSourceProvider
+    {
+        [Import]
+        internal ITextStructureNavigatorSelectorService NavigatorService { get; set; }
+
+        public ICompletionSource TryCreateCompletionSource(ITextBuffer textBuffer)
+        {
+            return new TestCompletionSource(this, textBuffer);
+        }
+    }
+}
